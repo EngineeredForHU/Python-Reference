bdab19a6fba291557d6ba765d50420f1b9492d22
Angel Perez <24.apbusiness@gmail.com>
2025-08-04 14:34:56 -0700

    added more functionality to to_do_list.py. 1. formatted the output to be more readable. 2. included a quantity functionality.

--- CorePython/Loops/EnumerateExamples.py
+++ CorePython/Loops/EnumerateExamples.py
@@ -1,6 +1,6 @@
 # This python file is meant to showcase the Enumerate function within python
 # What is an enumerate function?
-
+# The enumerate function helps keep track of the index in the sequence being traversed
 li = ['angel','perez','GIGI']
 
 for i,name in enumerate(li):
--- CorePython/PracticeCode/To_Do_List.py
+++ CorePython/PracticeCode/To_Do_List.py
@@ -5,25 +5,31 @@ grocery_list = []
 def add_to_list ():
     print("Add items to grocery list:")
     item = input("What item would you like to add? ")
+    quantity = int(input("Enter Quantity: "))
     price = float(input("How much does it cost? "))
 
     item_list = {
         'item': item,
-        'price': price
+        'price': price,
+        'quantity':quantity
     }
     grocery_list.append(item_list)
     print("Added item successfully!")
 
 def display_info(finished_grocery_list):
-    print("Item List:")
+    print(f"{'Qty':<5} {'Item':<15} {'Price':<10}")
+    print("-" * 45)
     for i, item in enumerate(finished_grocery_list,0):
-        print(f"{item['item']}: ${item['price']} ")
+        print(f"{item['quantity']:<5} {item['item']:<15} ${item['price']:<10}")
     add_total(finished_grocery_list)
 
 def add_total(total_cost):
     total_cost_of_items = 0
     for i in total_cost:
-        total_cost_of_items += i['price']
+        if i['quantity'] > 1:
+            total_cost_of_items += i['quantity'] * i['price']
+        else:
+            total_cost_of_items += i['price']
     print(f'Total cost: ${total_cost_of_items}')
 
 
