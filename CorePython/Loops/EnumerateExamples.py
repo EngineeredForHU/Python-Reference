d1d686670a4c67c89f284b83b550908676f4d23d
Angel Perez <24.apbusiness@gmail.com>
2025-08-04 13:50:19 -0700

    updated

---
+++ CorePython/Loops/EnumerateExamples.py
@@ -0,0 +1,7 @@
+# This python file is meant to showcase the Enumerate function within python
+# What is an enumerate function?
+
+li = ['angel','perez','GIGI']
+
+for i,name in enumerate(li):
+    print(f'{i}:{name}')
