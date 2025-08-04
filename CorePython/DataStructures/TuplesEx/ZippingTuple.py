4ed7af53d57ad6f185d2eef7b16573265e5debea
Angel Perez <24.apbusiness@gmail.com>
2025-08-04 13:56:09 -0700

    updated

---
+++ CorePython/DataStructures/TuplesEx/ZippingTuple.py
@@ -0,0 +1,9 @@
+# This file will showcase the examples of unpacking or zipping a tuple
+
+# Got a list of names and ages
+names = ['Angel','Gigi','Juan','AR']
+ages = [24,23,30,23]
+
+# Zipping them together
+zipped = list(zip(names,ages))
+print(zipped)
