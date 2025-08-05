26498822877cca9abbf461de88220665a420317f
Angel Perez <24.apbusiness@gmail.com>
2025-08-05 09:14:22 -0700

    Added comments to explain code.

---
+++ CorePython/OOP/Inheritance/First_example.py
@@ -0,0 +1,23 @@
+# This file will showcase a few examples of inheritance
+
+# Will first create a class called 'device' that reads in a device
+class Device:
+    def __init__(self, name, connected_by):
+        self.name = name
+        self.connected_by = connected_by
+        self.connected = True
+
+    def __str__(self):
+        return f'Device {self.name} ({self.connected_by})'
+
+    def disconnect(self):
+        self.connected = False
+        print('Disconnected.')
+
+# Now we will showcase how inheritance works
+# It's important to notice how super() is being used here
+# Instead of defining another init we can inherit what we need from the Device class with super().__init__(VAR1,..)
+class Printer(Device):
+    def __init__(self, name, connected_by, capacity):
+        super().__init__(name, connected_by)
+        self.capacity = capacity
