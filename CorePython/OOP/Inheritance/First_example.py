aaa81e84f69c637eeb84f5678ab819134e7ac970
Angel Perez <24.apbusiness@gmail.com>
2025-08-05 09:35:29 -0700

    Added inheritance folder to show examples of inheritance

--- CorePython/OOP/Inheritance/First_example.py
+++ CorePython/OOP/Inheritance/First_example.py
@@ -8,19 +8,38 @@ class Device:
         self.connected = True
 
     def __str__(self):
-        return f'Device {self.name} ({self.connected_by})'
+        return f'Device: {self.name}, Connected by:({self.connected_by})'
 
     def disconnect(self):
         self.connected = False
         print('Disconnected.')
 
+
+
 # Now we will showcase how inheritance works
 # It's important to notice how super() is being used here
-# Instead of defining another init we can inherit what we need from the Device class with super().__init__(VAR1,..)
-
+# Instead of defining another init we can inherit what we need from the parent function 'Device' class with super().__init__(VAR1,..)
 class Printer(Device):
     def __init__(self, name, connected_by, capacity):
+        # Using super().init() to inherit 'name', 'connected_by' from the parent class 'Device'
         super().__init__(name, connected_by)
         # There is a reason why we are assigning capacity twice here, 'capacity' is used to keep track of the total and 'remaining_pages' is current capacity
         self.capacity = capacity
         self.remaining_pages = capacity
+
+    def __str__(self):
+        return f'{super().__str__()} ({self.remaining_pages} pages remaining)'
+
+    def print_using_printer(self,pages):
+        if not self.connected:
+            print('your printer is not connected')
+            return
+        print(f'printing {pages} pages')
+        self.remaining_pages -= pages
+
+
+# Created an object called printer with the Printer class
+printer = Printer('Printer','USB',500)
+# here we are printing using the printer and printing 20 pages
+printer.print_using_printer(20)
+print(printer)
