1ddbac756c789545f20c0ab946c290bd4b5e3260
Angel Perez <24.apbusiness@gmail.com>
2025-08-06 14:55:36 -0700

    Added inheritance folder to show examples of inheritance

--- CorePython/Files/OpeningAFile.py
+++ CorePython/Files/OpeningAFile.py
@@ -1,3 +1,16 @@
 # Examples of writing/opening to a file in python
-f = open('data.txt','w')
-f.write('Hello\n')
+# For a more comprehensive explanation head to:
+# https://www.geeksforgeeks.org/python/file-handling-python/
+
+# Running this file will create a file in the current working directory
+# Just be aware!
+
+
+# There are 5 modes. (creating, opening, reading, writing, and closing)
+# The file variable opens the 'data.txt' with the writing mode indicated by the 'w'.
+file = open('data.txt','w')
+# We then run the file variable with .write() to write the things we want
+file.write('Hello\n')
+
+
+print(f'Mode: {file.mode}')
--- CorePython/Loops/EnumerateExamples.py
+++ CorePython/Loops/EnumerateExamples.py
@@ -3,5 +3,13 @@
 # The enumerate function helps keep track of the index in the sequence being traversed
 li = ['angel','perez','GIGI']
 
+# Notice how this for-enumerate starts at zero, there will be times when you want to start at one
+print("Starting at 0")
 for i,name in enumerate(li):
     print(f'{i}:{name}')
+
+# Here is an example starting the index at 1
+print("\nStarting at 1")
+# The second parameter is the starting point, and if there is no explicit starting point python starts at 0
+for i, name in enumerate(li, 1):
+    print(f'{i}: {name}')
